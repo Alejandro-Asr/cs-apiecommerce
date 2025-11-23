6b65c416e9bfc8302a2a0e01f787bec6164f4a2d
ALEXW11\asanc <asanchez7584@gmail.com>
2025-11-23 08:51:02 +0100

    final version jwt

--- obj/Debug/net8.0/ApiEcommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99fe0bc61a8d6bcb724ab93af3f805a31a42a92b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
